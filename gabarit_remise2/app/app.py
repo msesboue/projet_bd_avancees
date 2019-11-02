0c3c3c1330133585ae5e38faf2e34d9283d7a96d
smanzambi <sean.manzambi@insa-rouen.fr>
2019-11-02 18:35:01 -0400

    Correction index() de la route /

--- gabarit_remise2/app/app.py
+++ gabarit_remise2/app/app.py
@@ -10,17 +10,12 @@ app = Flask("VeloEpicurien")
 MONGODB_HOST = os.environ["MONGODB_PORT_27017_TCP_ADDR"]
 MONGODB_PORT = 27017
 client = MongoClient(MONGODB_HOST, MONGODB_PORT)
+
 db = client.appdb
-print(client.database_names())
-print(client.appdb.collection_names())
-for x in client.appdb.restaurant.find():
-    print(x)
+
 @app.route("/")
 def index():
-    _items = db.find()
-    items = [items for items in _items]
-
-    return render_template("index.html", items=items)
+    return render_template("index.html")
 
 @app.route("/nb_restaurant", methods=["GET"])
 def get_nb_restaurant():
