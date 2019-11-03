90474bda09bc6e3f6e00eee054351cc56422d5d2
Sesboue Matthias <matthias.sesboue@insa-rouen.fr>
2019-11-03 14:52:51 -0500

    modifs pour n'avoir q'une seule route

--- gabarit_remise2/app/app.py
+++ gabarit_remise2/app/app.py
@@ -2,11 +2,8 @@ import os
 from flask import Flask, redirect, url_for, request, render_template
 from pymongo import MongoClient, GEO2D
 
-
 app = Flask("VeloEpicurien")
 
-# To change accordingly 
-#print(os.environ)
 MONGODB_HOST = os.environ["MONGODB_PORT_27017_TCP_ADDR"]
 MONGODB_PORT = 27017
 client = MongoClient(MONGODB_HOST, MONGODB_PORT)
@@ -20,21 +17,17 @@ db.restaurants.create_index([("geometry.coordinates", GEO2D)])
 def index():
     return render_template("index.html")
 
-@app.route("/nb_restaurant", methods=["GET"])
-def get_nb_restaurant():
+@app.route("/heartbeat", methods=["GET"])
+def heartbeat():
     nb_restaurant = client.appdb.restaurants.find().count()
-    
-    return render_template("index.html", nb_restaurant=nb_restaurant)
 
-@app.route("/distance_totale", methods=["GET"])
-def get_distance_totale():
     dist_pipeline = [
         {"$group": {"_id": '',"total_dist": { "$sum": "$properties.Longueur" }}}, 
         {"$project": {"_id": 0,"total_dist": "$total_dist"}}
     ]
     distance = list(db.pistes.aggregate(dist_pipeline))
 
-    return render_template("index.html", distance_totale=distance[0]['total_dist'])
+    return render_template("index.html", nb_restaurant=nb_restaurant, distance_totale=distance[0]['total_dist'])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
