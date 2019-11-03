3cc0e8a9359afd6765360171c7d0e39758812041
Sesboue Matthias <matthias.sesboue@insa-rouen.fr>
2019-11-02 23:37:16 -0400

    v1 1 bd prêt

--- gabarit_remise2/app/app.py
+++ gabarit_remise2/app/app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, redirect, url_for, request, render_template
-from pymongo import MongoClient
+from pymongo import MongoClient, GEO2D
 
 
 app = Flask("VeloEpicurien")
@@ -13,20 +13,28 @@ client = MongoClient(MONGODB_HOST, MONGODB_PORT)
 
 db = client.appdb
 
+db.pistes.create_index([("geometry.coordinates", GEO2D)])
+db.restaurants.create_index([("geometry.coordinates", GEO2D)])
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
 @app.route("/nb_restaurant", methods=["GET"])
 def get_nb_restaurant():
-    nb_restaurant = client.appdb.restaurant.find().count()
-    #print(nb_restaurant)
+    nb_restaurant = client.appdb.restaurants.find().count()
+    
     return render_template("index.html", nb_restaurant=nb_restaurant)
 
 @app.route("/distance_totale", methods=["GET"])
 def get_distance_totale():
-    distance_totale = "À faire"
-    return render_template("index.html", distance_totale=distance_totale)
+    dist_pipeline = [
+        {"$group": {"_id": '',"total_dist": { "$sum": "$properties.Longueur" }}}, 
+        {"$project": {"_id": 0,"total_dist": "$total_dist"}}
+    ]
+    distance = list(db.pistes.aggregate(dist_pipeline))
+
+    return render_template("index.html", distance_totale=distance[0]['total_dist'])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
