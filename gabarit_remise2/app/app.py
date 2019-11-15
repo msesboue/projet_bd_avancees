f0ce91b46cbf774992d20431c7fc8ee9eb812eb2
Sesboue Matthias <matthias.sesboue@insa-rouen.fr>
2019-11-15 12:25:47 -0500

    maj app comme en cours --> fonctionnelle

--- gabarit_remise2/app/app.py
+++ gabarit_remise2/app/app.py
@@ -1,25 +1,38 @@
 import os
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, jsonify
 from pymongo import MongoClient, GEO2D
+from py2neo import Graph
 
-app = Flask("VeloEpicurien")
+if "production" in os.environ:
+    mongo_server_uri = "mongo"
+    neo4j_server_uri = "neo"
+else:
+    mongo_server_uri = "localhost"
+    neo4j_server_uri = "localhost"
 
-MONGODB_HOST = os.environ["MONGODB_PORT_27017_TCP_ADDR"]
 MONGODB_PORT = 27017
-client = MongoClient(MONGODB_HOST, MONGODB_PORT)
+NEO4J_PORT = 7687
+APP_PORT = 5000
 
-db = client.appdb
+app = Flask("Vélo Épicurien")
+mongo_client = MongoClient("{}:{}".format(mongo_server_uri, MONGODB_PORT))
+neo4j_graph = Graph("http://{}".format(neo4j_server_uri), auth={"neo4j","projetBD"})
+
+# MONGODB_HOST = os.environ["MONGODB_PORT_27017_TCP_ADDR"]
+# client = MongoClient(MONGODB_HOST, MONGODB_PORT)
+
+db = mongo_client.velo_epicurien
 
 db.pistes.create_index([("geometry.coordinates", GEO2D)])
 db.restaurants.create_index([("geometry.coordinates", GEO2D)])
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+@app.route("/", methods=["GET"])
+def homepage():
+    return "Bonjour et bienvenu dans notre application de vélo épicurien"
 
 @app.route("/heartbeat", methods=["GET"])
 def heartbeat():
-    nb_restaurant = client.appdb.restaurants.find().count()
+    nb_restaurant = mongo_client.velo_epicurien.restaurants.find().count()
 
     dist_pipeline = [
         {"$group": {"_id": '',"total_dist": { "$sum": "$properties.Longueur" }}}, 
@@ -27,8 +40,11 @@ def heartbeat():
     ]
     distance = list(db.pistes.aggregate(dist_pipeline))
 
-    return render_template("index.html", nb_restaurant=nb_restaurant, distance_totale=distance[0]['total_dist'])
+    return jsonify({
+        "nb_restaurants": nb_restaurant,
+        "total_path_length": distance[0]['total_dist']
+    })
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run("0.0.0.0", APP_PORT, debug=True)
 
